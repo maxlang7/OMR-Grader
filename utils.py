650ff9dc1a709b6c7208bb3abcc0524c698dc6b5
Max Langhorst <max@langhorst.com>
2020-12-13 16:09:38 -0500

    Can't have neighborhood of zero or negative so we have to add one not subtract one

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def get_threshold(im, constant):
     w, h = im.shape
     neighborhood = int(w*h/15000)
     if neighborhood % 2 == 0:
-        neighborhood = neighborhood - 1
+        neighborhood = neighborhood + 1
     blurred = cv.GaussianBlur(im, (1, 1), 0)
     blurred = cv.bilateralFilter(blurred,5,50,50)
     threshold = cv.adaptiveThreshold(blurred,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
