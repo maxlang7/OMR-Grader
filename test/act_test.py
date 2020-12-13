c616b62646f5fd3ff3ee23ff449464a696e2a146
Max Langhorst <max@langhorst.com>
2020-12-13 16:07:12 -0500

    Fixing answers and image file name

--- test/act_test.py
+++ test/act_test.py
@@ -7,7 +7,7 @@ from wand.image import Image
 
 class ExampleActTests(unittest.TestCase):
     #turn both to true to see images.
-    verbose_mode = False
+    verbose_mode = True
     debug_mode = verbose_mode
 
     def test_act_page1(self):
@@ -16,32 +16,32 @@ class ExampleActTests(unittest.TestCase):
         data = json.loads(jsonData)
 
         expected_answers = [ ('A J D H B J B J C J B G A ' +
-                              'H D H B J C H C F B H A F ' +
+                              'H D H B H C H A F B H C F ' +
                               'D G A F B H B H A F A F C ' +
                               'J C G D J C J D H B F D J ' +
-                              'A F B G C F D G C G A F C ' +
+                              'A F D G C F D G C H A F C ' +
                               'H D G D J A F B H D').split(' '),
                             
                              ('D H E F E H E H A K ' +
                               'C K B H B H D F D F ' +
-                              'B H A H B G E H C G ' +
-                              'C K B H D J A H B F ' +
+                              'B H A H B G A H C G ' +
+                              'E K B H D J A F B F ' +
                               'E K D G D J B G A F ' +
-                              'D H B K E J A K C J').split(' '),
+                              'E H B K E K A K E J').split(' '),
                             
                              ('A J C H B J A ' +
-                              'H B J A J B J ' +
-                              'B H B F - F A ' +
-                              'G D J C H C J ' +
+                              'H B J A H C J ' +
+                              'B H B F D F A ' +
+                              'G D J C G C J ' +
                               'C G A G A J D ' +
                               'H B J A H').split(' '),
                             
                              ('C G D G C F A ' +
                               'F D J C F B F ' +
-                              'C F B H B H D ' +
+                              'C F B H B J D ' +
                               'F B J B H A J ' +
-                              'C F B G D H A ' +
-                              'H B H D J').split(' ')
+                              'C F B G D J A ' +
+                              'H B F D J').split(' ')
                           ]
                         
         for i, graded_results in enumerate(data['boxes']):
@@ -90,7 +90,7 @@ class ExampleActTests(unittest.TestCase):
             
     def test_act_page1b(self):
         grader = g.Grader()
-        jsonData = grader.grade('test/images/act_test1b.jpeg', self.debug_mode, self.verbose_mode, 1.0, 'act', 1)
+        jsonData = grader.grade('test/images/act_test1b.png', self.debug_mode, self.verbose_mode, 1.0, 'act', 1)
         data = json.loads(jsonData)
         expected_answers = [ ('A J D H B J B J C J B G A ' +
                               'H D H B J C H C F B H A F ' +
