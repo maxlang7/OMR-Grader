ec537df831384864c0143a1baa90431e41eb5ac1
Max Langhorst <max@langhorst.com>
2021-03-07 14:41:42 -0500

    added downloader for wufoo csv exports.

--- test/csv/post_wufoo_csv.py
+++ test/csv/post_wufoo_csv.py
@@ -1,10 +1,15 @@
 import csv
+import os
+import urllib.parse
 import click
+from numpy.lib.function_base import extract
 import requests
+import urllib.request
 @click.command()
 @click.option('-f', required=True)
+@click.option('--mode', required=True, default='post')
 @click.option('--url', default='http://ec2-54-174-87-221.compute-1.amazonaws.com:8000/v1/grader', required=True)
-def import_csv_file(f, url):
+def import_csv_file(f, mode, url):
     with open(f, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         #[print(str(i)+':'+value) for i, value in enumerate(line.keys())]
@@ -26,7 +31,18 @@ def import_csv_file(f, url):
             'Field24': dict_values[9],
             'Field5': dict_values[10]
             }
-            requests.post(url, dict)
+            if mode == 'post':
+                requests.post(url, dict)
+            elif mode == 'download':
+                for i in range(29, 36):
+                    imagelink = extract_parens(dict_values[i])
+                    if imagelink == '':
+                        continue
+                    path = urllib.parse.urlparse(imagelink).path
+                    extension = os.path.splitext(path)[1]
+                    filename = f'/Users/maxanna/Documents/AutoGrader/OMR-Grader/test/images/image{dict_values[9]}{i}.{extension}'
+                    urllib.request.urlretrieve(imagelink, filename)
+
 
            
             
