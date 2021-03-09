72c10032cecc6feaaf5353c5cd9ba79113235a11
Max Langhorst <max@langhorst.com>
2021-03-08 21:07:57 -0500

    We had two csv headers that were the same, so we switched methods from dictReader to reader.

--- test/csv/post_wufoo_csv.py
+++ test/csv/post_wufoo_csv.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 import os
 import urllib.parse
 import click
@@ -11,36 +12,39 @@ import urllib.request
 @click.option('--url', default='http://ec2-54-174-87-221.compute-1.amazonaws.com:8000/v1/grader', required=True)
 def import_csv_file(f, mode, url):
     with open(f, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
+        reader = csv.reader(csvfile)
         #[print(str(i)+':'+value) for i, value in enumerate(line.keys())]
         for line in reader:
-            dict_values = list(line.values())
+            if line[0] == 'Entry Id':
+                continue
             dict = {'HandshakeKey': 'SPPNscores2020',
-            'Field6': line['For which exam are you submitting answers?'],
-            'Field17-url': extract_parens(dict_values[29]),
-            'Field49-url': extract_parens(dict_values[30]),
-            'Field8-url': extract_parens(dict_values[31]),
-            'Field9-url': extract_parens(dict_values[32]),
-            'Field10-url': extract_parens(dict_values[33]),
-            'Field11-url': extract_parens(dict_values[34]),
-            'Field12-url': extract_parens(dict_values[35]),
-            'Field15': dict_values[26],
-            'Field44': dict_values[27],
-            'Field45': dict_values[28],
-            'Field23': dict_values[8],
-            'Field24': dict_values[9],
-            'Field5': dict_values[10]
+            'Field6': line[26],
+            'Field17-url': extract_parens(line[30]),
+            'Field49-url': extract_parens(line[31]),
+            'Field8-url': extract_parens(line[32]),
+            'Field9-url': extract_parens(line[33]),
+            'Field10-url': extract_parens(line[34]),
+            'Field11-url': extract_parens(line[35]),
+            'Field12-url': extract_parens(line[36]),
+            'Field15': line[27], #SAT version
+            'Field44': line[28], #ACT version
+            'Field45': line[29],
+            'Field23': line[8],
+            'Field24': line[9],
+            'Field5': line[10]
             }
             if mode == 'post':
-                requests.post(url, dict)
+                print(dict)
+                resp = requests.post(url, dict)
+                print(resp)
             elif mode == 'download':
                 for i in range(29, 36):
-                    imagelink = extract_parens(dict_values[i])
+                    imagelink = extract_parens(line[i])
                     if imagelink == '':
                         continue
                     path = urllib.parse.urlparse(imagelink).path
                     extension = os.path.splitext(path)[1]
-                    filename = f'/Users/maxanna/Documents/AutoGrader/OMR-Grader/test/images/image{dict_values[9]}{i}.{extension}'
+                    filename = f'/Users/maxanna/Documents/AutoGrader/OMR-Grader/test/images/image{line[9]}{i}.{extension}'
                     urllib.request.urlretrieve(imagelink, filename)
 
 
